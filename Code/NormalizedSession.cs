261c73ce37850fe6969046a3ca67e41416df55c2
Lewis Hammond <bubblemapgaming@gmail.com>
2025-09-14 17:11:22 +0100

    Use League Pic

--- Code/NormalizedSession.cs
+++ Code/NormalizedSession.cs
@@ -112,7 +112,7 @@ namespace iRacingTVController
 
 			isDirtTrack = trackType.Contains( "dirt", StringComparison.OrdinalIgnoreCase );
 
-			seriesLogoTextureUrl = $"https://ir-core-sites.iracing.com/members/member_images/series/seriesid_{IRSDK.session.WeekendInfo.SeriesID}/logo.jpg";
+			seriesLogoTextureUrl = $"https://ir-core-sites.iracing.com/members/member_images/series/leagueid_{IRSDK.session.WeekendInfo.LeagueID}/logo.jpg";
 			trackLogoTextureUrl = DataApi.GetTrackLogoUrl( trackID.ToString() );
 
 			numForwardGears = IRSDK.session.DriverInfo.DriverCarGearNumForward;
